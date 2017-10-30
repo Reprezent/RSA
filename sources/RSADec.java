1d5b6beb64c9bedd0fc530c7052a2420744ddf65
J.T. Liso <jtliso417@gmail.com>
2017-10-29 21:00:11 -0400

    Update RSADec.java

--- sources/RSADec.java
+++ sources/RSADec.java
@@ -4,7 +4,7 @@
 
 class RSADec {
 	public RSADec(String [] args){
-		//creates a cmd line arg parser for key gen (true)
+		//creates a cmd line arg parser for enc/dec (false)
 		CommandLineArgParser cmd = new CommandLineArgParser(args, false);
 
 		try{
