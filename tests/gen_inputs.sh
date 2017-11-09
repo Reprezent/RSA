57ce44d6a88da3cda9339c056f11d6831264eea2
Richard Riedel <richardalexriedel@yahoo.com>
2017-11-09 09:16:58 -0500

    fixed scripts

--- tests/gen_inputs.sh
+++ tests/gen_inputs.sh
@@ -2,6 +2,7 @@
 input_dir="inputs"
 increment=2
 length=2
+mkdir -p $input_dir
 while [ $length -le 4094 ]; do
     printf "Generating input of size %8d" $length
     ../random_bits $length > $input_dir/`printf "%04d" $length`.in
--- tests/gen_keys.sh
+++ tests/gen_keys.sh
@@ -3,6 +3,7 @@ priv_dir="private_keys"
 pub_dir="public_keys"
 increment=16
 length=40
+mkdir -p $priv_dir $pub_dir
 while [ $length -le 4096 ]; do
     printf "Generating key of size %d" $length
     ../rsa-keygen -p $pub_dir/$length.public -s $priv_dir/$length.private -n $length
